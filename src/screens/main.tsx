e2dafedb34e167ee61b0909488f1ff6faa8920cc
Muxammadbobur <bobur0172@gmail.com>
2023-06-28 17:45:14 +0500

    setup

---
+++ src/screens/main.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import { Text, Box, Center } from 'native-base'
+
+const MainScreen = () => {
+  return (
+    <Center backgroundColor={'blue.50'} px={4} flex={1}>
+      <Box>
+        <Text>Hello World</Text>
+      </Box>
+    </Center>
+  )
+}
+
+export default MainScreen
